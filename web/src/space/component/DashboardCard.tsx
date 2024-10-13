a1808e38058a11b5ecfa41a8e0932899c4d59ff5
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-10-13 21:15:17 +0530

    add: space menu in dashboard

--- web/src/space/component/DashboardCard.tsx
+++ web/src/space/component/DashboardCard.tsx
@@ -1,7 +1,8 @@
 import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../ui/components/Button';
-import { List } from 'lucide-react';
+import { Copy, Dock, List, SquarePen, TriangleAlert } from 'lucide-react';
 import { toast } from 'sonner';
+import { useState } from 'react';
 
 type PropsType = {
   spaceName: string;
@@ -9,9 +10,11 @@ type PropsType = {
 };
 const DashboardCard = ({ spaceName, feedbackCount }: PropsType) => {
   const navigate = useNavigate();
-  const feedbackUrl = `http://localhost:3000/feedback/${spaceName}`;
+  const [menuStatus, setMenuStatus] = useState<boolean>(false);
+  const feedbackUrl: string = `http://localhost:3000/feedback/${spaceName}`;
+
   return (
-    <div className="border-accent mx-auto flex w-full max-w-[22rem] flex-col gap-y-2 overflow-hidden rounded-md border-[2px] px-4 py-3 transition-all ease-in-out">
+    <div className="border-accent mx-auto flex w-full max-w-[22rem] flex-col gap-y-2 rounded-md border-[2px] px-4 py-3 transition-all ease-in-out">
       <div className={'flex flex-row justify-between'}>
         <div
           className="cursor-pointer text-xl font-semibold capitalize hover:text-gray-300 hover:underline"
@@ -19,15 +22,65 @@ const DashboardCard = ({ spaceName, feedbackCount }: PropsType) => {
         >
           {spaceName}
         </div>
-        <Button
-          type={'button'}
-          variant={'secondary'}
-          className={
-            'w-fit rounded-full bg-transparent p-0 hover:bg-transparent md:p-0'
-          }
-          icon={<List className={'h-4 w-4 text-white hover:text-gray-300'} />}
-          onClick={() => toast.info('feature not implemented yet!')}
-        />
+        <div className={'relative inline-block'}>
+          <div>
+            <Button
+              type={'button'}
+              variant={'secondary'}
+              className={
+                'w-fit rounded-full border-none bg-transparent p-0 hover:bg-transparent md:p-0'
+              }
+              icon={
+                <List className={'h-4 w-4 text-white hover:text-gray-300'} />
+              }
+              onClick={() => setMenuStatus((prevState) => !prevState)}
+            />
+          </div>
+          {menuStatus && (
+            <div
+              className={
+                'menu border-accent absolute right-0 z-20 w-40 rounded-md border-[2px] bg-gray-950 py-1'
+              }
+            >
+              <Button
+                type={'button'}
+                variant={'outlineB'}
+                text={'Manage Space'}
+                icon={<Dock className={'h-4 w-4'} />}
+                className={
+                  'hover:bg-accent justify-start rounded-none border-none bg-transparent'
+                }
+                onClick={() => navigate(`/space/${spaceName}`)}
+              />
+              <Button
+                type={'button'}
+                variant={'outlineB'}
+                text={'Edit Space'}
+                icon={<SquarePen className={'h-4 w-4'} />}
+                className={`hover:bg-accent justify-start rounded-none border-none bg-transparent`}
+                onClick={() => toast.info('feature not implemented yet!')}
+              />
+              <Button
+                type={'button'}
+                variant={'outlineB'}
+                text={'Copy link'}
+                icon={<Copy className={'h-4 w-4'} />}
+                className={`hover:bg-accent justify-start rounded-none border-none bg-transparent`}
+                onClick={() =>
+                  toast.success('Feedback page link copy to clipboard')
+                }
+              />
+              <Button
+                type={'button'}
+                variant={'danger'}
+                text={'Delete Space'}
+                icon={<TriangleAlert className={'h-4 w-4'} />}
+                className={`justify-start rounded-none border-none bg-transparent`}
+                onClick={() => toast.info('feature not implemented yet!')}
+              />
+            </div>
+          )}
+        </div>
       </div>
       <p className={'text-sm text-gray-300'}>
         Total Feedbacks: {feedbackCount}
