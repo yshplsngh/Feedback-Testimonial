96a9597eb1043e727303a33a8bcbf7ee7dda0213
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-10-27 12:26:26 +0530

    code refactor

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -2,15 +2,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT, NODE_ENV, USER_SESSION_SECRET, CLIENT_SECRET, CLIENT_ID } =
-  process.env;
-
 export default {
-  PORT: PORT || 4001,
-  NODE_ENV: NODE_ENV,
+  PORT: process.env.PORT || 4001,
+
+  NODE_ENV: process.env.NODE_ENV,
+
   USER_SESSION_SECRET:
-    USER_SESSION_SECRET || 'MicXqgdng5pXTHLvzHiYv8HssdivNNmgcwGcUVeEDEs=',
-  CLIENT_ID: CLIENT_ID || 'HERE_GOES_YOUR_GOOGLE_CLIENT_ID',
-  CLIENT_SECRET: CLIENT_SECRET || 'HERE_GOES_YOUR_GOOGLE_CLIENT_SECRET',
+    process.env.USER_SESSION_SECRET ||
+    'MicXqgdng5pXTHLvzHiYv8HssdivNNmgcwGcUVeEDEs=',
+
+  CLIENT_ID: process.env.CLIENT_ID || 'HERE_GOES_YOUR_GOOGLE_CLIENT_ID',
+
+  CLIENT_SECRET:
+    process.env.CLIENT_SECRET || 'HERE_GOES_YOUR_GOOGLE_CLIENT_SECRET',
+
   GOOGLE_CALLBACK: 'http://localhost:4000/api/auth/google/redirect',
 };
