4b885493fb38cd0a5b0a2a0c18125bfee32b9800
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-11-07 13:12:18 +0530

    fix deployment image

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,11 +5,5 @@ const server = createServer();
 
 server.listen(config.PORT, () => {
   console.log(`Node connected on ${config.PORT} ✅`);
-  console.log(
-    config.NODE_ENV === 'development'
-      ? config.DEV_GOOGLE_CALLBACK
-      : config.PROD_GOOGLE_CALLBACK,
-  );
-  console.log(config.PROD_GOOGLE_CALLBACK);
-  console.log(config.DEV_GOOGLE_CALLBACK);
+  console.log(config.NODE_ENV === 'development' ? 'development' : 'production');
 });
