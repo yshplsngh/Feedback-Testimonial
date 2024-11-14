1a5eb57c128f1126f3b9a5229e041a3f0d3c0459
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-11-14 11:29:12 +0530

    fix:fixing cookies error

--- api/src/server.ts
+++ api/src/server.ts
@@ -21,7 +21,10 @@ export const createServer = (): Express => {
   app.use(express.json());
   app.use(
     cors({
-      origin: ['http://localhost:3000'],
+      origin: [
+        'https://testimonial.yshplsngh.in',
+        'https://feedback-testimonial.vercel.app',
+      ],
       credentials: true,
     }),
   );
