72740730da19f9c1786837395710bb2c33b524d1
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-11-14 12:05:43 +0530

    fix:testing dummy cookies

--- api/src/auth/index.ts
+++ api/src/auth/index.ts
@@ -38,6 +38,7 @@ export default function authRoutes(app: Express): void {
          * when true: cookie set over a secure channel like HTTPS only.
          * when auto: cookie set over an HTTP also.
          */
+        sameSite: 'none',
         secure: config.NODE_ENV === 'development' ? 'auto' : true,
         /** when true, cookie can't be accessed through client-side JavaScript.*/
         httpOnly: true,
