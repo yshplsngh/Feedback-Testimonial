913488388cc1b191e9f19474f29aaab77fedc900
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-11-14 11:50:19 +0530

    fix:testing dummy cookies

--- api/src/user/index.ts
+++ api/src/user/index.ts
@@ -4,10 +4,9 @@ export default function userRoutes(app: Express) {
   app.get('/api/user', (req: Request, res: Response) => {
     res.cookie('testing', 'testing', {
       httpOnly: true, // Prevents JavaScript access to the cookie
-      secure: process.env.NODE_ENV === 'production', // Required for HTTPS
+      secure: true, // Required for HTTPS
       sameSite: 'strict', // Protects against CSRF
-      maxAge: 24 * 60 * 60 * 1000, // Optional: cookie expiration (24 hours in this example)
-      path: '/', // Cookie path
+      maxAge: 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(req.user || { message: 'yeah no login user' });
   });
